0ae0ee6216a6751150733d67cc6cd3435ea887cd
juan071825 <juanmateoalvarez@gmail.com>
2026-05-15 19:42:24 +0200

    feat(App.java): :sparkles: Añadir importación de paquete receptivo.

--- app/src/main/java/edu/teamrocket/ricksyBusiness/App.java
+++ app/src/main/java/edu/teamrocket/ricksyBusiness/App.java
@@ -27,6 +27,7 @@ package edu.teamrocket.ricksyBusiness;
 
 import edu.teamrocket.ricksyBusiness.paymentMethods.*;
 import edu.teamrocket.ricksyBusiness.services.*;
+import edu.teamrocket.ricksyBusiness.receptivo.*;
 
 public class App {
     
