b4b327775b9f60cd53a80c09af195f32d007b28a
juan071825 <juanmateoalvarez@gmail.com>
2026-05-16 19:31:55 +0200

    feat(UfosPark.java): :sparkles: Completar métodos dispatch, getUfoOf y toString.

--- app/src/main/java/edu/teamrocket/ricksyBusiness/services/UfosPark.java
+++ app/src/main/java/edu/teamrocket/ricksyBusiness/services/UfosPark.java
@@ -1,7 +1,10 @@
 package edu.teamrocket.ricksyBusiness.services;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.List;
 
 import edu.teamrocket.ricksyBusiness.paymentMethods.CreditCard;
 
@@ -15,20 +18,56 @@ public class UfosPark implements GuestDispatcher{
 
     public UfosPark(){};
 
-    public void add(String ovni){
-        this.fleet.put(ovni, null);
+
+    public Map<String, String> getFleet(){
+        return this.fleet;
     }
 
-
-
+    public void add(String ovni){
+        getFleet().putIfAbsent(ovni, null);
+    }
 
     @Override
     public void dispatch(CreditCard card) {
 
-        card.pay(500);
+        List<String> freeUfos = new ArrayList<>();
 
-        
+        for(Map.Entry<String, String> ufo : getFleet().entrySet()){
+            if(ufo.getValue() == null){
+                freeUfos.add(ufo.getKey());
+            }
+        }
 
+        if(freeUfos.size() > 0 && card.pay(500) == true){
+            Integer freeUfoIndex = ThreadLocalRandom.current().nextInt(0, freeUfos.size());
+            getFleet().replace(getFleet().get(freeUfos.get(freeUfoIndex)), null, card.owner());
+        }
     }
+
+    public String getUfoOf(String name){
+        for(Map.Entry<String, String> ufo : getFleet().entrySet()){
+            if(ufo.getValue() == name){
+                return ufo.getKey();
+            }
+        }
+        return null;
+    }
+
+
+    public String toString() {
+
+        StringBuilder ufosPark = new StringBuilder();
+        ufosPark.append("Listado de ufos y usuarios que los están utilizando\n");
+        getFleet().forEach((ufo, usuario) ->
+            ufosPark.append("ufo: ")
+                    .append(ufo)
+                    .append("   usuario: ")
+                    .append(usuario)
+                    .append("\n")
+        );
+        
+        return ufosPark.toString();
+    }
+
 
 }
